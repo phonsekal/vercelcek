6420bba2bf221d080ccd493612940ec48d2de75c
phonsekal <33015680+phonsekal@users.noreply.github.com>
2026-07-09 12:03:28 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -162,14 +162,9 @@ def home():
                     </a>
                     <div class="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
                         <div class="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
-                            <a class="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100" href="blog.dedesaputra.com">Blog</a>
-                            <a class="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100" href="blog.dedesaputra.com/about">About</a>
+                            <a class="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100" href="https://blog.dedesaputra.com">Blog</a>
+                            <a class="hover:text-primary-500 dark:hover:text-primary-400 m-1 font-medium text-gray-900 dark:text-gray-100" href="https://blog.dedesaputra.com/about">About</a>
                         </div>
-                        <button aria-label="Search">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="hover:text-primary-500 dark:hover:text-primary-400 h-6 w-6 text-gray-900 dark:text-gray-100">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
-                            </svg>
-                        </button>
                         <div class="flex items-center">
                             <div class="relative inline-block text-left">
                                 <div class="hover:text-primary-500 dark:hover:text-primary-400 flex items-center justify-center">
